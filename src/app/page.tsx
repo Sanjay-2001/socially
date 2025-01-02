0e1fb4790fb1aed4bf2bcb04ac044b11d037e183
SanjayDutta <sanjaydutta976@gmail.com>
2025-01-02 13:09:24 +0530

    suggested users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import CreatePost from "@/components/CreatePost";
 import ModeToggle from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
+import WhoToFollow from "@/components/WhoToFollow";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 
@@ -11,7 +12,7 @@ export default async function Home() {
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
       <div className="hidden lg:block lg:col-span-4 sticky top-20">
-        Who to follow
+        <WhoToFollow />
       </div>
     </div>
   );
