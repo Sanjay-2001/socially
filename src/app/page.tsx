82c01c8bf144ef596c340a05da0911876ea57243
SanjayDutta <sanjaydutta976@gmail.com>
2025-01-02 09:11:10 +0530

    auth and db sync

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,10 @@ import ModeToggle from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="m-4">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign in</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle />
-      <Button variant={"secondary"}>Click me</Button>
+      <h1>home page content</h1>
     </div>
   );
 }
